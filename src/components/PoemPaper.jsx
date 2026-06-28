f742952e9ab548779c6d3a89034790b87bada6f6
Nikunj <mwnjele@gmail.com>
2026-06-28 19:36:21 +0530

    Copy poem!

--- src/components/PoemPaper.jsx
+++ src/components/PoemPaper.jsx
@@ -1,13 +1,53 @@
-import { Container, Grid, Paper, Typography } from '@mui/material';
+import { Container, Grid, Paper, Typography, IconButton, Snackbar } from '@mui/material';
 
 import AnimatedButton from './AnimatedButton';
 
 import { motion } from "framer-motion";
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { Fragment } from 'react';
 
+import { FaCopy, FaCheck } from "react-icons/fa6";
+
+import { dateStringToNumerialString } from '../data/Utils'
+
 export default function PoemPaper({ poem }) {
-    console.log(poem);
+    const [copied, setCopied] = useState(false);
+    const [state, setState] = useState({
+        open: false,
+        vertical: 'top',
+        horizontal: 'right',
+        title: 'Copied!'
+    });
+    const { vertical, horizontal, open, title } = state;
+
+    const handleCopy = async () => {
+        const header = `"${poem.title}" by ${poem.signature}, ${dateStringToNumerialString(poem.date)}\n\n`;
+
+        let poemBody = poem.poem.map(stanza => stanza.join("\n")).join("\n\n");
+
+        poemBody += `\r\n      ${'-'.repeat(poem.signatureLength)}${poem.signature}`;
+
+        const finalPoem = header + poemBody;
+
+        try {
+            await navigator.clipboard.writeText(finalPoem);
+            
+            setCopied(true);
+            setState({ ...state, title: "Copied to clipboard!" });
+            setState({ ...state, open: true });
+
+            setTimeout(() => {
+                setCopied(false);
+                setState({ ...state, open: false });
+            }, 2000);
+
+        } catch (err) {
+            setState({ ...state, title: "Copy Failed!" });
+        }
+        setState({ ...state, open: true });
+    };
+
     return (
         <>
             <Container maxWidth="md">
@@ -21,7 +61,11 @@ export default function PoemPaper({ poem }) {
                             <Typography variant="h7" sx={{ textAlign: "center", mt: { xs: "14px", md: "4px" }, fontSize: "0.9rem" }}> Written on { poem.date } </Typography>
                         </Grid>
                         <Grid size={2}>
-                            
+                            <motion.div whileHover={{ scale: 1.02, transition: { duration: 0.2 } }} whileTap={{ scale: 0.9 }} >
+                                <IconButton sx={{ textAlign: "right", mt: { xs: "14px", md: "14px" } }} onClick={ handleCopy }>
+                                    { copied ? <FaCheck /> : <FaCopy />}
+                                </IconButton>
+                            </motion.div>
                         </Grid>
                     </Grid>
 
@@ -46,6 +90,12 @@ export default function PoemPaper({ poem }) {
                         ~ { poem.signature }
                     </Typography>
                 </Paper>
+                <Snackbar
+                    anchorOrigin={{ vertical, horizontal }}
+                    open={ open }
+                    message={ title }
+                    key={vertical + horizontal}
+                />
             </Container>
         </>
     );
